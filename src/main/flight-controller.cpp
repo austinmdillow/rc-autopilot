4fa3bcd3013d666ba3eca8629c3faa4c9cb80989
austinmdillow <austinmdillow@gmail.com>
2019-10-25 23:16:20 -0400

    rf24 is now working needed to fix addresses

--- src/main/flight-controller.cpp
+++ src/main/flight-controller.cpp
@@ -7,9 +7,53 @@
 #include "duet.cpp"
 #include "AcPID.cpp"
 //#include <PID_v1.h>
-#include "flight-radio.cpp"
+//#include "flight-radio.cpp"
 #include <PID_Em.h>
-//#include "Iaircraft.cpp"
+//#include <RF24.h>
+#include <SPI.h>
+//#include <RF24.h>
+byte addressesQ[][6] = {"1Node","2Node"};
+
+struct Remote {
+  byte target_id = 1;
+  byte pot_1;
+  byte pot_2;
+  byte stick_L_x;
+  byte stick_L_y;
+  bool stick_L_b;
+  byte stick_R_x;
+  byte stick_R_y;
+  bool stick_R_b;
+  bool estop;
+  bool switch_1;
+  bool switch_2;
+  bool trim_U;
+  bool trim_D;
+  bool trim_L;
+  bool trim_R;
+};
+
+Remote data_in;
+//RF24 flightRadio(9,10);
+
+/**
+void setupRadio() {
+  flightRadio.begin();
+  flightRadio.setPALevel(RF24_PA_LOW);
+  flightRadio.openWritingPipe(addressesQ[0]);
+  flightRadio.openReadingPipe(1,addressesQ[1]);
+  flightRadio.startListening();
+}
+
+void readRadio() {
+  if(flightRadio.available()){
+    // Variable for the received timestamp
+    while (flightRadio.available()) {                                   // While there is data ready
+      flightRadio.read( &data_in, sizeof(data_in) );             // Get the payload
+    }
+  }
+}
+*/
 
 void manualControl();
 void pidSetup();
@@ -42,7 +86,7 @@ struct Error {
 };
 
 void manualControl() {
-  Serial.print(data_rx.target_id);
+  //Serial.print(data_rx.target_id);
 }
 
 void performTakeoff() {
--- src/main/flight-radio.cpp
+++ src/main/flight-radio.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <RF24.h>
-byte addresses[][6] = {"1Node","2Node"};
+
+//uint64_t addresses[][6] = {0xF0F0F0F066};
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+const byte masterAddress[5] = {'T','X','a','a','a'};
 
 struct Remote {
   byte target_id = 1;
@@ -22,3 +25,22 @@ struct Remote {
 };
 
 Remote data_rx;
+
+RF24 flightRadio(9,10);
+
+void setupRadio() {
+  flightRadio.begin();
+  flightRadio.setPALevel(RF24_PA_LOW);
+  flightRadio.openWritingPipe(masterAddress[0]);
+  flightRadio.openReadingPipe(1,slaveAddress[1]);
+  flightRadio.startListening();
+}
+
+void readRadio() {
+  if(flightRadio.available()){
+    // Variable for the received timestamp
+    while (flightRadio.available()) {                                   // While there is data ready
+      flightRadio.read( &data_rx, sizeof(data_rx) );             // Get the payload
+    }
+  }
+}
